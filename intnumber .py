79333eaa36938ea2969e3125268d58dd467fcdbf
Amos Omariba <amomariba@gmail.com>
2024-07-12 12:22:02 +0300

    added other files

---
+++ intnumber .py
@@ -0,0 +1,27 @@
+# integer_number = 123
+# float_number = 1.23
+
+# new_number = integer_number + float_number
+
+# # display new value and resulting data type
+# print("Value:",new_number)
+# print("Data Type:",type(new_number))
+
+
+import random
+
+print(random.randrange(10, 20))
+
+list1 = ['a', 'b', 'c', 'd', 'e']
+
+# get random item from list1
+print(random.choice(list1))
+
+# Shuffle list1
+#random.shuffle(list1)
+
+# Print the shuffled list1
+#print(list1)
+
+# Print random element
+#print(random.random())
---
+++ quiz.py
@@ -0,0 +1,22 @@
+"""Function accepts username input"""
+
+number = input("Enter queue number: ")
+#number = int(input("Enter queue number: "))
+print("Your queue number is,", number)
+
+name = input("Enter your valid name: ")
+print("Your real name is,", name)
+
+age = int(input("Enter your actual age: "))
+print("Your real age is,", age)
+
+Id_Number = int(input("Enter your id number: "))
+print("Your Identification number is,", Id_Number)
+
+phone_number = int(input("Enter your correct phone number: "))
+print("Your phone number is,", phone_number)
+
+Location = input("Enter where you stay: ")
+print("Your actual location is,", Location)
+
+print("Thank You for your responses!!!!!")
